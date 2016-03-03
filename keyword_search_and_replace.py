0dba6217bf7d722b4ce23f965cf5d6b62913a3c1
Jonathan Zerox <jonathanzerox@hotmail.com>
2016-03-03 17:09:31 +0300

    Added directory traversing feature to search for the file even in deep nested directories...
    I added a lot of enhancements, including searching of the file directories recursively...

--- keyword_search_and_replace.py
+++ keyword_search_and_replace.py
@@ -3,27 +3,38 @@ Program: Python Script that searches a file for specific keywords and replaces t
 Reason: I had to replace database settings on many php files so i decided to automate the process with .py
 Author: Jonathan Zerox
 Date: Thurs - 3rd - March - 2015
+CS Concepts Used: Recursion
 """
 
 import os, sys
 
-old_file = ''
-new_file = ''
+def walk_dir(cur_dir):
 
-for files in os.listdir(os.curdir):
-    if(files[-3:] == 'php'):
-        with(open(files, 'r')) as open_file:
-            file_contents = open_file.read()
-            split_data = str(file_contents).replace('Connections/connect.php', 'config/db_settings.php').split(' ')
+    old_file = ''   # variable to hold filename for the original file
+    new_file = ''   # variable to hold filename for the new file/file with replaced keyword
 
-            old_file = open_file.name
-            another_file = open('another_file.txt', 'w') 
-            another_file.writelines(" ".join(split_data))
+    for file_name in os.listdir(cur_dir):
+        if(file_name[-3:] == 'php' and os.path.isfile(cur_dir+"/"+file_name)):
 
-            new_file = another_file.name
+            with(open(os.path.abspath(cur_dir+"/"+file_name), 'r')) as open_file:
+                file_contents = open_file.read()
+                split_data = str(file_contents).replace('Connections/connect.php', 'config/db_settings.php').split(' ')
 
-            another_file.close()
+                old_file = open_file.name
+                another_file = open(os.path.abspath(cur_dir+'/'+'another_file.txt'), 'w')
+                another_file.writelines(" ".join(split_data))
 
-        os.remove(old_file)
-        os.rename(new_file, old_file)
-        
+                new_file = another_file.name
+
+                another_file.close()
+
+            # after copying the contents file1 to file2 we then rename file2 to the name of file1 then remove file1 and keep file2(whose name has been changed to file1)
+            os.remove(old_file)
+            os.rename(new_file, old_file)
+
+        if(os.path.isdir(cur_dir+'/'+file_name)):
+            walk_dir(cur_dir+'/'+file_name)
+
+if __name__ == '__main__':
+    walk_dir(os.curdir)
+
